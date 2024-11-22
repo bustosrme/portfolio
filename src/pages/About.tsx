bd55ca5404aaed323041526855c0be9d833f90d6
Mauro Bustos Roldan <bustosrme@gmail.com>
2024-11-22 10:09:38 -0300

    fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -9,7 +9,7 @@ function About() {
     return (
         <motion.section
             ref={ref}
-            className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 font"
+            className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 mt-20 md:mt-0"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.175 }}
